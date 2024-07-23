b13243baeaee0b809a0347762e492b5f0bf7130d
yosuad <yosuad@hotmail.com>
2024-07-23 08:24:03 -0500

    avance7

--- Blog_videojuegos/includes/aside.php
+++ Blog_videojuegos/includes/aside.php
@@ -4,13 +4,25 @@
 <aside id="sidebar">
 
     <?php if(isset($_SESSION['usuario'])) : ?>
-    <div id="usuario-logueado" class="bloque">
-        <h3><?php echo $_SESSION['usuario']['nombre']. ' '.$_SESSION['usuario']['apellidos']; ?></h3>
-    </div>
+        <div id="usuario-logueado" class="bloque">
+            <h3><?php echo $_SESSION['usuario']['nombre']. ' '.$_SESSION['usuario']['apellidos']; ?></h3>
+            <!-- BOTONES -->
+            <a href="cerrar.php" class="boton boton-verde">Crear entradas</a>
+            <a href="cerrar.php" class="boton">Crear categoria</a>
+            <a href="cerrar.php" class="boton boton-naranja">Mis datos</a>
+            <a href="cerrar.php" class="boton boton-rojo">Cerrar sesión</a>
+        </div>
     <?php endif; ?>
 
     <div id="login" class="bloque">
         <h3>Identificate</h3>
+
+        <?php if(isset($_SESSION['error_login'])) : ?>
+            <div class="alerta alerta-error">
+                <?php echo $_SESSION['error_login'] ?>
+            </div>
+        <?php endif; ?>
+
         <form action="login.php" method="POST">
             <label for="email">Email</label>
             <input type="email" name="email">
--- Blog_videojuegos/login.php
+++ Blog_videojuegos/login.php
@@ -4,6 +4,12 @@ require_once 'includes/conexion.php';
 
 //RECOGER DATOS DEL FORMULARIO
 if (isset($_POST)) {
+
+    // BORRAR ERROR ANTIGUO
+    if (isset($_SESSION['error_login'])) {
+        unset($_SESSION['error_login']);
+    }
+
     $email = trim($_POST['email']);
     $password = $_POST['password'];    
 }
@@ -22,11 +28,7 @@ if ($login && mysqli_num_rows($login) == 1) {
        // UTILIZAR UNA SESIÓN PARA GUARDAR LOS DATOS DEL USUARIO LOGUEADO
        $_SESSION['usuario'] = $usuario;
 
-       if (isset($_SESSION['error_login'])) {
-           unset($_SESSION['error_login']);
-       }
-
-
+      
     }else{
         // SI ALGO FALLA ENVIAR UNA SESIÓN CON EL FALLO
         $_SESSION['error_login'] = "Login Incorrecto";
