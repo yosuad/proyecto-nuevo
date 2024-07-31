0cb1fcfa3b1726f90afccd61d31b9e1dce62a20f
yosuad <yosuad@hotmail.com>
2024-07-31 07:57:45 -0500

    avance13

--- Blog_videojuegos/entrada.php
+++ Blog_videojuegos/entrada.php
@@ -11,12 +11,22 @@
                  header("Location: index.php");
             }
         ?>
+
+     
             <h1><?=$entrada_actual['titulo'] ?></h1>
             <a href="categoria.php?id=<?=$entrada_actual['categoria_id'] ?>"><h2><?=$entrada_actual['categoria'] ?></h2></a>
             
 
-            <h4><?=$entrada_actual['fecha'] ?></h4>
+            <h4><?=$entrada_actual['fecha'] ?> | <?=$entrada_actual['usuario'] ?></h4>
             <p><?=$entrada_actual['descripcion'] ?></p>
+
+            <?php if (isset($_SESSION["usuario"]) && $_SESSION['usuario']['id'] == $entrada_actual['usuario_id']) : ?>
+                <a href="editar-entrada.php" class="boton boton-verde">Editar entrada</a>
+                <a href="borrar-entrada.php?id=<?=$entrada_actual['id']?>" class="boton">Borrar entrada</a>
+            <?php endif; ?>
+            
+          
+
         </div>        
       
     <?php require_once 'includes/footer.php'; ?>  
